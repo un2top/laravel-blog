3794de2327b0b2d1583dc32665584c5cb704fc6e
User <un2top@yandex.ru>
2023-06-11 01:58:13 +0300

    posts

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,6 +7,14 @@
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
             <li class="nav-item">
+                <a href="{{route('admin.post.index')}}" class="nav-link">
+                    <i class="nav-icon far fa-clipboard"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
                 <a href="{{route('admin.category.index')}}" class="nav-link">
                     <i class="nav-icon fas fa-list"></i>
                     <p>
