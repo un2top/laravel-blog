5b14bcc96e00de874c7b8d1b6c25b1f64c43a15b
User <un2top@yandex.ru>
2023-06-13 23:54:11 +0300

    main page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,10 +10,20 @@ class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
     protected $table = 'posts';
     protected $guarded = false;
 
-    public function tags(){
+    public function tags()
+    {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function likedUsers(){
+        return $this->belongsToMany(User::class, 'post_user_likes', 'post_id', 'user_id');
     }
 }
