378c3c28a16a7067975edc388980c668211aa293
User <un2top@yandex.ru>
2023-06-13 22:20:45 +0300

    coments

--- resources/views/personal/comment/index.blade.php
+++ resources/views/personal/comment/index.blade.php
@@ -24,7 +24,43 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-
+                <div class="col-6">
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Название</th>
+                                    <th colspan="2" class="text-center">Действия</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($comments as $comment)
+                                    <tr>
+                                        <td>{{$comment->id}}</td>
+                                        <td>{{$comment->message}}</td>
+                                        <td class="text-center"><a href="{{route('personal.comment.edit', $comment->id)}}"
+                                                                   class="text-success"><i class="fas fa-pencil-alt"></i></a></td>
+                                        <td class="text-center">
+                                            <form action="{{route('personal.comment.delete', $comment->id)}}"
+                                                  method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i class="fas fa-trash text-danger"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
             </div>
         </div><!-- /.container-fluid -->
     </section>
